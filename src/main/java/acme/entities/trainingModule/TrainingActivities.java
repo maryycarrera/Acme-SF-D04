d4f32b98db676778af0880eac7bd8549ee439414
Igancio Blanquero <ignacio.blanquero@gmail.com>
2024-03-03 20:38:59 +0100

    feat: Task047

---
+++ src/main/java/acme/entities/trainingModule/TrainingActivities.java
@@ -0,0 +1,51 @@
+
+package acme.entities.trainingModule;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+
+public class TrainingActivities extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private final static long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "PG-[A-Z]{1,2}-[0-9]{4}")
+	private String				code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private LocalDateTime		creationMoment;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				details;
+
+	@NotBlank
+	private DifficultyLevel		difficultyLevel;
+
+	private LocalDateTime		updateMoment;
+
+	@URL
+	private String				link;
+
+	@Min(0)
+	private Integer				estimatedTotalTime;
+
+}
