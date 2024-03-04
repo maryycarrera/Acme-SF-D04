c2241b3fdc9349d72e30e29a7740cc1e56ddc8aa
Igancio Blanquero <ignacio.blanquero@gmail.com>
2024-03-04 19:11:17 +0100

    feat: Task049 Developer Dashboard

---
+++ src/main/java/acme/forms/DeveloperDashboard.java
@@ -0,0 +1,29 @@
+
+package acme.forms;
+
+import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeveloperDashboard extends AbstractForm {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	Integer						totalTrainingModulesWithUpdateMoment;
+	Integer						totalTrainingSessionsWithLink;
+	Double						averageTrainingModuleTime;
+	Double						deviationTrainingModuleTime;
+	Double						minimumTrainingModuleTime;
+	Double						maximumTrainingModuleTime;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
