ec297a5736e49f1ee223bdfa6e791ffd207e305f
joagongan <joagongan@alum.us.es>
2024-03-05 22:48:48 +0100

    Task 024 complete

---
+++ src/main/java/acme/entities/objective/Objective.java
@@ -0,0 +1,80 @@
+
+package acme.entities.objective;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.accounts.Principal;
+import acme.entities.projects.Project;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Objective extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				instantiationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+	@NotNull
+	private Priority			priority;
+
+	@NotNull
+	private Boolean				status;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				startTimeDuration;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				finishTimeDuration;
+
+	@URL
+	private String				link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@OneToOne(optional = false)
+	private Principal			principal;
+
+	@NotNull
+	@Valid
+	@OneToOne(optional = false)
+	private Project				project;
+}
