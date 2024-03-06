1089cca0404e29f2a2e52ddf8c2ff0ea1c4cf91c
maryycarrera <marcartal1@alum.us.es>
2024-03-06 17:16:56 +0100

    Task 031 - Modified to primitive types
    Tipos de los atributos modificados a tipos primitivos en casos
    necesarios

--- src/main/java/acme/entities/projects/Project.java
+++ src/main/java/acme/entities/projects/Project.java
@@ -5,6 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -13,7 +14,6 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
-import acme.client.data.datatypes.Money;
 import acme.roles.Manager;
 import lombok.Getter;
 import lombok.Setter;
@@ -43,16 +43,17 @@ public class Project extends AbstractEntity {
 	private String				abstractDescription;
 
 	@NotNull
-	private Boolean				hasFatalErrors;
+	private boolean				hasFatalErrors;
 
 	@NotNull
-	private Money				cost;
+	@Min(0)
+	private int					cost;
 
 	@URL
 	private String				link;
 
 	@NotNull
-	private Boolean				draftMode;
+	private boolean				draftMode;
 
 	// Derived attributes -----------------------------------------------------
 
