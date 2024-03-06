de0e9edebd75eba8180088cd836f8bf7aa2d6186
maryycarrera <marcartal1@alum.us.es>
2024-03-06 15:49:17 +0100

    banner arreglado

--- src/main/java/acme/entities/banners/Banner.java
+++ src/main/java/acme/entities/banners/Banner.java
@@ -1,26 +1,19 @@
 
 package acme.entities.banners;
 
-import java.time.LocalDate;
-import java.time.Period;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
-import javax.validation.constraints.PastOrPresent;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
-import acme.client.data.accounts.Administrator;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -42,13 +35,11 @@ public class Banner extends AbstractEntity {
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
-	@Past
-	private LocalDate			startDate;
+	private Date				startDate;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
-	@PastOrPresent
-	private LocalDate			finishDate;
+	private Date				finishDate;
 
 	@NotBlank
 	@URL
@@ -64,18 +55,6 @@ public class Banner extends AbstractEntity {
 
 	// Derived attributes -----------------------------------------------------
 
-
-	@Transient
-	public Integer displayPeriod() {
-		Period p = Period.between(this.startDate, this.finishDate);
-		return p.getDays() / 7;
-	}
-
 	// Relationships ----------------------------------------------------------
 
-
-	@NotNull
-	@Valid
-	@ManyToOne(optional = false)
-	private Administrator administrator;
 }
