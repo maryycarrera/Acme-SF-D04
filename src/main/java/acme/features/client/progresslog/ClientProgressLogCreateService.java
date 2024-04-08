cb0f581a5bb7d1a3a886795122c281559170cbc7
nataliaaolmo <natolmvil@alum.us.es>
2024-04-08 16:55:02 +0200

    feat: manejar excepción double

--- src/main/java/acme/features/client/progresslog/ClientProgressLogCreateService.java
+++ src/main/java/acme/features/client/progresslog/ClientProgressLogCreateService.java
@@ -65,7 +65,12 @@ public class ClientProgressLogCreateService extends AbstractService<Client, Prog
 
 	@Override
 	public void validate(final ProgressLog object) {
+		//TODO: la fecha me deja meterla si es 2022
 		assert object != null;
+		if (!super.getBuffer().getErrors().hasErrors("completeness")) {
+			super.state(object.getCompleteness() > 0, "completeness", "client.progress-log.form.error.negative-completeness");
+			super.state(object.getCompleteness() <= 100, "completeness", "client.progress-log.form.error.major100-completeness");
+		}
 	}
 
 	@Override
