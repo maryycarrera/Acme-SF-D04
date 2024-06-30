9c197c281a55141478f12d934410e86ac8c0dd0b
Igancio Blanquero <ignacio.blanquero@gmail.com>
2024-06-30 12:51:16 +0200

    TrainingSession delete.safe test done and AnyTrainingModuleShow fixed

--- src/main/java/acme/features/any/trainingModule/AnyTrainingModuleShowService.java
+++ src/main/java/acme/features/any/trainingModule/AnyTrainingModuleShowService.java
@@ -1,16 +1,12 @@
 
 package acme.features.any.trainingModule;
 
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.client.data.accounts.Any;
 import acme.client.data.models.Dataset;
 import acme.client.services.AbstractService;
-import acme.client.views.SelectChoices;
-import acme.entities.projects.Project;
 import acme.entities.trainingmodules.TrainingModule;
 
 @Service
@@ -51,16 +47,7 @@ public class AnyTrainingModuleShowService extends AbstractService<Any, TrainingM
 	public void unbind(final TrainingModule object) {
 		assert object != null;
 
-		Collection<Project> projects;
-		SelectChoices choices;
-		Dataset dataset;
-
-		projects = this.repository.findAllProjects();
-		choices = SelectChoices.from(projects, "title", object.getProject());
-
-		dataset = super.unbind(object, "code", "creationMoment", "details", "difficultyLevel", "updateMoment", "link", "estimatedTotalTime", "draftMode");
-		dataset.put("project", choices.getSelected().getKey());
-		dataset.put("projects", choices);
+		Dataset dataset = super.unbind(object, "code", "creationMoment", "details", "difficultyLevel", "updateMoment", "link", "estimatedTotalTime", "draftMode", "project");
 
 		super.getResponse().addData(dataset);
 	}
