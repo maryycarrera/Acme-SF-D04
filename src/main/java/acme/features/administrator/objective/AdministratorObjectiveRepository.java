0932f7afb0f6f4ec52c166d4eb0046e00dd3b6e4
joagongan <joagongan@alum.us.es>
2024-07-06 21:31:14 +0200

    Repository fix

--- src/main/java/acme/features/administrator/objective/AdministratorObjectiveRepository.java
+++ src/main/java/acme/features/administrator/objective/AdministratorObjectiveRepository.java
@@ -1,16 +1,11 @@
 
 package acme.features.administrator.objective;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import acme.client.data.accounts.Administrator;
 import acme.client.repositories.AbstractRepository;
 
 @Repository
 public interface AdministratorObjectiveRepository extends AbstractRepository {
-
-	@Query("SELECT a FROM Administrator a WHERE a.id = :id")
-	Administrator findOneAdministratorById(int id);
 
 }
